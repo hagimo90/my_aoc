79b22c9930d4ce9589a898aa4d76eb32fb783a8e
hagimo90 <hagimo90@gmail.com>
2022-12-17 23:57:14 +0100

    first function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use num::complex::Complex;
+
+fn mendelbrot_at_point(cx:f64,cy:f64,iters:usize) -> usize{
+
+    let mut z = Complex::new(0.0,0.0);
+    let c = Complex::new(cx,cy);
+    for i  in 0..=iters {
+        if z.norm()> 2.0 {
+            return i;
+        }
+        z = z*z +c;
+    }
+    return iters;
+}
 fn main() {
     println!("Hello, world!");
 }
