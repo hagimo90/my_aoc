73b94dcf100a3bd37f09d0252b9f8548fd4d0bb9
hagimo90 <hagimo90@gmail.com>
2022-12-18 00:31:35 +0100

    first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use num::complex::Complex;
-
 fn mendelbrot_at_point(cx:f64,cy:f64,iters:usize) -> usize{
 
     let mut z = Complex::new(0.0,0.0);
@@ -12,6 +11,57 @@ fn mendelbrot_at_point(cx:f64,cy:f64,iters:usize) -> usize{
     }
     return iters;
 }
+
+fn calculate_mandelbrot(iters:usize,x_min:f64,x_max:f64,y_min:f64,y_max:f64,width:usize,height:usize)-> Vec<Vec<usize>> {
+
+    let set_height = y_max - y_min;
+    let set_width = x_max - x_min;
+    let mut rows = Vec::new();
+
+    for img_y in 0..height {
+        let mut row = Vec::new();
+        for img_x in 0..width {
+            let width_ = width as f64;
+            let height_ = height as f64;
+            let img_y_ = img_y as f64;
+            let img_x_ = img_x as f64;
+
+            let cx= x_min + set_width*(img_x_ /width_ );
+            let cy = y_min + set_height*(img_y_ /height_ );
+            let escaped_at = mendelbrot_at_point(cx,cy,iters);
+            row.push(escaped_at);
+         
+    }
+    rows.push(row);
+}
+
+
+    rows
+}
+
+fn render_mandelbrot (escape_vals :Vec<Vec<usize>> ) {
+    for row in escape_vals {
+        let mut line = String::with_capacity(row.len());
+        for column in row {
+            let val = match column {
+                0..=2 => ' ',
+                2..=5 => '.',
+                5..=10 => ':',
+                11..=30 => '*',
+                30..=100 => 'x',
+                100..=200 => '%',
+                200..=400 => '$',
+                400..=700 => '#',
+                _ => '@',
+            };
+            line.push(val);
+
+            
+        }
+        println!("{}",line);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mandelbrot = calculate_mandelbrot(1000,-2.0,1.0,-1.1,1.1,50,50);
+    render_mandelbrot(mandelbrot );
 }
